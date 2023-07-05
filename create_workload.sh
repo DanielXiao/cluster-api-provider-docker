7b2c8ce632d6a33c8cd6df669d1295176a99834b
Yifeng Xiao <yifengx@vmware.com>
2023-07-05 12:40:14 +0800

    Support flannel CNI and change k8s version

--- create_workload.sh
+++ create_workload.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-export KUBERNETES_VERSION=v1.26.0
+export KUBERNETES_VERSION=v1.26.3
 export CLUSTER_NAME=kubecontest
 export CONTROL_PLANE_MACHINE_COUNT=1
 export WORKER_MACHINE_COUNT=1
+export CNI_NAME=flannel
 
-kubectl create configmap antrea-crs-configmap --from-file=antrea.yaml
+kubectl create configmap ${CNI_NAME}-crs-configmap --from-file=${CNI_NAME}.yaml
 clusterctl generate cluster kubecontest --from templates/cluster-template.yaml > cluster.yaml
 kubectl apply -f cluster.yaml
