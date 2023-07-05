02c6e383e0a9552de746f160ef995606ddc6fafa
Yifeng Xiao <yifengx@vmware.com>
2023-07-05 16:24:47 +0800

    Switch back to antrea CNI

--- create_workload.sh
+++ create_workload.sh
@@ -4,7 +4,7 @@ export KUBERNETES_VERSION=v1.26.3
 export CLUSTER_NAME=kubecontest
 export CONTROL_PLANE_MACHINE_COUNT=1
 export WORKER_MACHINE_COUNT=1
-export CNI_NAME=flannel
+export CNI_NAME=antrea
 
 kubectl create configmap ${CNI_NAME}-crs-configmap --from-file=${CNI_NAME}.yaml
 clusterctl generate cluster kubecontest --from templates/cluster-template.yaml > cluster.yaml
